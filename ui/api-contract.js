701dfde2142a1f01605401f9b72593a7d1e7e065
Abhinav Goyal <abhinavgoyal245@gmail.com>
2023-10-13 19:09:22 +0530

    updated

--- ui/api-contract.js
+++ ui/api-contract.js
@@ -9,7 +9,7 @@ function getStringAfterEquals(inputString) {
 
 async function readBuildFile(branchName) {
   if (!branchName) return;
-  const url = `https://api.github.com/repos/ondc-official/mobility-specification/contents/ui/build.js?ref=${branchName}`;
+  const url = `https://api.github.com/repos/ondc-official/ONDC-RET-Specifications/contents/ui/build.js?ref=${branchName}`;
 
   try {
     const response = await fetch(url, {
@@ -57,8 +57,8 @@ async function fetchRequest(url){
 
 async function loadContracts() {
   //fetch branches & tags from repo
-  const BRANCHES_URL= "https://api.github.com/repos/ondc-official/mobility-specification/branches";
-  const TAGS_URL= "https://api.github.com/repos/ondc-official/mobility-specification/tags";
+  const BRANCHES_URL= "https://api.github.com/repos/ondc-official/ONDC-RET-Specifications/branches";
+  const TAGS_URL= "https://api.github.com/repos/ondc-official/ONDC-RET-Specifications/tags";
                   
   let response1, response2;
   response1 = await fetchRequest(BRANCHES_URL)
