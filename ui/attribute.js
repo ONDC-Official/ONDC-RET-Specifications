aec5c4f88a4d246e23434a76d4130e2311fa3deb
Abhinav Goyal <abhinavgoyal245@gmail.com>
2023-10-19 16:53:51 +0530

    ui fixes

--- ui/attribute.js
+++ ui/attribute.js
@@ -16,37 +16,28 @@ function updateSetsAttribute() {
 }
 
 function loadAttributes(data) {
-  try {
-    // To fix: attributes are getting appended to list on branch change.
-    var elements = document.getElementsByClassName("test");
-    while (elements.length > 0) {
-      elements[0].parentNode.removeChild(elements[0]);
-    }
-    attributes = data;
-    var attributesDropDown = document.getElementById("attribute-dropdown");
-    attributesDropDown.innerHTML = "";
-
-    Object.keys(attributes).forEach(function (key) {
-      var option = document.createElement("option");
-      option.text = key;
-      attributesDropDown.add(option);
-    });
-    const indexKey = Object.keys(attributes);
-    addAttributeSets(indexKey[0]);
-  } catch (error) {
-    console.log(error);
+  // To fix: attributes are getting appended to list on branch change.
+  var elements = document.getElementsByClassName("test");
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
   }
+  attributes = data;
+  var attributesDropDown = document.getElementById("attribute-dropdown");
+  attributesDropDown.innerHTML = "";
+
+  Object.keys(attributes).forEach(function (key) {
+    var option = document.createElement("option");
+    option.text = key;
+    attributesDropDown.add(option);
+  });
+  const indexKey = Object.keys(attributes);
+  addAttributeSets(indexKey[0]);
 }
 
 function updateSets(value, option) {
   const object = attributes[value]?.attribute_set;
-  console.log("object[option]", object[option]["required_attributes"]);
-  flattenObject(
-    object[option],
-    null,
-    null,
-    object[option]?.required_attributes
-  );
+  console.log('object[option]',object[option]["required_attributes"]);
+  flattenObject(object[option],null,null,object[option]?.required_attributes);
 }
 
 function addAttributeSets(option) {
@@ -62,43 +53,44 @@ function addAttributeSets(option) {
 
   const firstKey = Object.keys(object)[0];
   const keyDetail = object[firstKey];
-  const requiredAttr = "required_attributes" in keyDetail;
-  flattenObject(keyDetail, null, null, keyDetail?.required_attributes);
+  const requiredAttr = 'required_attributes' in keyDetail
+  flattenObject(keyDetail,null,null,keyDetail?.required_attributes);
 }
 
-function flattenObject(obj, prefix = "", result = {}, requiredAttr) {
+function flattenObject(obj, prefix = "", result = {},requiredAttr) {
   if ("required" in obj) {
-    if (requiredAttr === undefined || requiredAttr.includes(prefix)) {
-      var table = document.getElementById("tableset");
-      const newRow = document.createElement("tr");
-      newRow.classList.add("test");
-      newRow.style.wordBreak = "break-all";
-      const cell1 = document.createElement("td");
-      const cell2 = document.createElement("td");
-      const cell3 = document.createElement("td");
-      const cell4 = document.createElement("td");
+    if(requiredAttr===undefined || requiredAttr.includes(prefix)){
+    var table = document.getElementById("tableset");
+    const newRow = document.createElement("tr");
+    newRow.classList.add("test");
+    newRow.style.wordBreak = "break-all";
+    const cell1 = document.createElement("td");
+    const cell2 = document.createElement("td");
+    const cell3 = document.createElement("td");
+    const cell4 = document.createElement("td");
 
-      cell1.textContent = prefix;
-      cell2.textContent = obj["required"];
-      cell3.textContent = obj["usage"];
-      cell4.textContent = obj["description"];
+    cell1.textContent = prefix;
+    cell2.textContent = obj["required"];
+    cell3.textContent = obj["usage"];
+    cell4.textContent = obj["description"];
 
-      newRow.appendChild(cell1);
-      newRow.appendChild(cell2);
-      newRow.appendChild(cell3);
-      newRow.appendChild(cell4);
+    newRow.appendChild(cell1);
+    newRow.appendChild(cell2);
+    newRow.appendChild(cell3);
+    newRow.appendChild(cell4);
 
-      table.appendChild(newRow);
+
+    table.appendChild(newRow);
     }
     return;
   }
   for (const key in obj) {
-    if (obj.hasOwnProperty(key) && key !== "required_attributes") {
+    if (obj.hasOwnProperty(key) && key!=='required_attributes') {
       const newKey = prefix ? prefix + "." + key : key;
       if (Array.isArray(obj[key])) {
         result[newKey] = obj[key];
       } else if (typeof obj[key] === "object" && obj[key] !== null) {
-        flattenObject(obj[key], newKey, result, requiredAttr);
+        flattenObject(obj[key], newKey, result,requiredAttr);
       } else {
         result[newKey] = obj[key];
       }
