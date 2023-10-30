bcdbe13e34805a03b5548900e66db351269ec8ea
Abhik Banerjee <abhik.banerjee@thewitslab.com>
2023-10-30 16:08:09 +0530

    Working state. Need to change the footnote.

--- ui/attribute.js
+++ ui/attribute.js
@@ -1,5 +1,53 @@
 var attributes;
 
+function updateAttribute() {
+  var example_set = document.getElementById("attribute-dropdown");
+  var selectedValue = example_set.value;
+  document.querySelectorAll(".content").forEach((div) => div.remove());
+  addAttributeSets(selectedValue);
+}
+
+function updateSetsAttribute() {
+  var attributesDropDown = document.getElementById("attribute-dropdown");
+  var example_set = document.getElementById("attribute-sets-dropdown");
+  document.querySelectorAll(".content").forEach((div) => div.remove());
+  var selectedValue = example_set.value;
+  updateSets(attributesDropDown.value, selectedValue);
+}
+
+function updateL1Attribute() {
+  var attributesDropDown = document.getElementById("attribute-dropdown");
+  var example_set = document.getElementById("attribute-sets-dropdown");
+  var l1Dropdown = document.getElementById("attribute-l1-dropdown");
+  document.querySelectorAll(".content").forEach((div) => div.remove());
+  var object =
+    attributes[attributesDropDown.value]?.attribute_set[example_set.value];
+  for (const each of l1Dropdown.value.split(".")) {
+    object = object[each];
+  }
+  console.log("Object to render", object);
+  object = removeParentAttributes(object);
+  if (
+    "required" in object[Object.keys(object).filter((e) => e !== "parent")[0]]
+  )
+    flattenObject(object, null, null, object?.required_attributes);
+}
+
+function removeParentAttributes(obj) {
+  var newObj = {};
+  for (const key in obj) {
+    if (
+      !["parent", "usage", "description", "reference", "required"].includes(
+        key
+      )
+       &&
+      !("parent" in obj[key])
+    )
+      newObj = { ...newObj, [key]: { ...obj[key] } };
+  }
+  return newObj;
+}
+
 function loadAttributes(data) {
   attributes = data;
   var attributesDropDown = document.getElementById("attribute-dropdown");
@@ -11,70 +59,100 @@ function loadAttributes(data) {
     attributesDropDown.add(option);
   });
   const indexKey = Object.keys(attributes);
-  addAttributeSets(attributes[indexKey[0]]?.attribute_set);
+  addAttributeSets(indexKey[0]);
 }
 
-function updateAttribute() {
-  var example_set = document.getElementById("attribute-dropdown");
-  var selectedValue = example_set.value;
-  addAttributeSets(attributes[selectedValue]?.attribute_set);
-}
+function updateSets(value, option) {
+  const object = attributes[value]?.attribute_set[option];
 
-function updateSetsAttribute() {
-  var attributesDropDown = document.getElementById("attribute-dropdown");
-  var example_set = document.getElementById("attribute-sets-dropdown");
+  var l1DropDown = document.getElementById("attribute-l1-dropdown");
+  l1DropDown.innerHTML = "";
 
-  const object =
-    attributes[attributesDropDown.value]?.attribute_set[example_set.value];
-  addAttributeSets(object, ["attribute-l1-dropdown", "attribute-l2-dropdown"]);
-}
+  const paths = generatePaths(object);
 
-function updateL1Attribute() {
-  var attributesDropDown = document.getElementById("attribute-dropdown");
-  var example_set = document.getElementById("attribute-sets-dropdown");
-  var l1Attribute = document.getElementById("attribute-l1-dropdown");
-  const object =
-    attributes[attributesDropDown.value]?.attribute_set[example_set.value];
-  addAttributeSets(object[l1Attribute.value], ["attribute-l2-dropdown"]);
-}
+  paths.forEach(function (key) {
+    var option = document.createElement("option");
+    option.text = key;
+    l1DropDown.add(option);
+  });
 
-function updateL2Attribute() {
-  var attributesDropDown = document.getElementById("attribute-dropdown");
-  var example_set = document.getElementById("attribute-sets-dropdown");
-  var l1Attribute = document.getElementById("attribute-l1-dropdown");
-  var l2Attribute = document.getElementById("attribute-l2-dropdown");
-  const object =
-    attributes[attributesDropDown.value]?.attribute_set[example_set.value];
-  addAttributeSets(object[l1Attribute.value][l2Attribute.value], [
-    "attribute-l2-dropdown",
-  ]);
-}
-
-function addAttributeSets(
-  data,
-  keyList = [
-    "attribute-sets-dropdown",
-    "attribute-l1-dropdown",
-    "attribute-l2-dropdown",
-  ]
-) {
-  var object = data;
-
-  for (each of keyList) {
-    var setsDropDown = document.getElementById(each);
-    setsDropDown.innerHTML = "";
-
-    Object.keys(object).forEach(function (key) {
-      var option = document.createElement("option");
-      option.text = key;
-      setsDropDown.add(option);
-    });
-
-    const firstKey = Object.keys(object)[0];
-    object = object[firstKey];
+  const firstKey = paths[0];
+  var keyDetail = object;
+  for (const each of firstKey.split(".")) {
+    keyDetail = keyDetail[each];
   }
 
-  flattenObject(object, null, null, object?.required_attributes);
+  console.log("Object to render", keyDetail);
+
+  // if ("required" in keyDetail)
+  flattenObject(keyDetail, null, null, keyDetail?.required_attributes);
+}
+
+function addAttributeSets(option) {
+  const object = attributes[option]?.attribute_set;
+  var setsDropDown = document.getElementById("attribute-sets-dropdown");
+  setsDropDown.innerHTML = "";
+
+  Object.keys(object).forEach(function (key) {
+    var option = document.createElement("option");
+    option.text = key;
+    setsDropDown.add(option);
+  });
+
+  var l1DropDown = document.getElementById("attribute-l1-dropdown");
+  l1DropDown.innerHTML = "";
+
+  const paths = generatePaths(object[Object.keys(object)[0]]);
+  console.log(
+    "PATH from addAttributeSets",
+    paths,
+    object[Object.keys(object)[0]]
+  );
+
+  paths.forEach(function (key) {
+    var option = document.createElement("option");
+    option.text = key;
+    l1DropDown.add(option);
+  });
+
+  const firstKey = paths[0];
+  var keyDetail = object[Object.keys(object)[0]];
+  for (const each of firstKey.split(".")) {
+    keyDetail = keyDetail[each];
+  }
+
+  console.log("Object to render", keyDetail);
+
+  if (
+    "required" in
+    keyDetail[Object.keys(keyDetail).filter((e) => e !== "parent")[0]]
+  )
+    flattenObject(keyDetail, null, null, keyDetail?.required_attributes);
+}
+
+function generatePaths(o, root = "", result = [], itr = 0) {
+  var ok = Object.keys(o);
+  const filteredKeys = ok.filter(
+    (e) =>
+      !["required", "description", "usage", "reference", "parent"].includes(e)
+  );
+
+  if (ok.includes("parent") && o["parent"] === true)
+    for (const each of filteredKeys) {
+      var p = root.length > 0 ? root + "." + each : each;
+
+      generatePaths(o[each], p, result, itr + 1);
+    }
+  const addRoute = ["required", "description", "usage", "reference"].every(
+    (e) => ok.includes(e)
+  );
+  if (addRoute && root.length > 0)
+    var actualRoot = root.split(".").slice(0, -1).join(".");
+  if (actualRoot && !result.includes(actualRoot)) {
+    result.push(actualRoot);
+  }
+
+  return result;
 }
 
 function flattenObject(obj, prefix = "", result = {}, requiredAttr) {
@@ -82,7 +160,7 @@ function flattenObject(obj, prefix = "", result = {}, requiredAttr) {
     if (requiredAttr === undefined || requiredAttr.includes(prefix)) {
       var table = document.getElementById("tableset");
       const newRow = document.createElement("tr");
-      newRow.classList.add("test");
+      newRow.classList.add("content");
       newRow.style.wordBreak = "break-all";
       const cell1 = document.createElement("td");
       const cell2 = document.createElement("td");
@@ -104,7 +182,11 @@ function flattenObject(obj, prefix = "", result = {}, requiredAttr) {
     return;
   }
   for (const key in obj) {
-    if (obj.hasOwnProperty(key) && key !== "required_attributes") {
+    if (
+      obj.hasOwnProperty(key) &&
+      key !== "required_attributes" &&
+      key !== "parent"
+    ) {
       const newKey = prefix ? prefix + "." + key : key;
       if (Array.isArray(obj[key])) {
         result[newKey] = obj[key];
